69d2fa57113e9d877023a244dbfd95759fcd2377
jreleaserbot <jreleaser@kordamp.org>
2022-01-05 00:04:52 +0000

    mychecksum v0.0.9

--- Formula/mychecksum.rb
+++ Formula/mychecksum.rb
@@ -1,17 +1,17 @@
-# Generated with JReleaser 0.10.0 at 2022-01-04T00:33:10.665974704Z
+# Generated with JReleaser 0.10.0 at 2022-01-05T00:04:38.740941654Z
 class Mychecksum < Formula
   desc "ref-java-cli -- Reference Java CLI program - Jumpstart Repo Template"
   homepage "https://github.com/rrajesh1979/ref-java-cli"
-  version "0.0.7"
+  version "0.0.9"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/rrajesh1979/ref-java-cli/releases/download/v0.0.7/mychecksum-0.0.7-linux-x86_64.zip"
-    sha256 "2d5e71470fb4899ee9e67404e588412a47117f000b7eaf9a025d1ff8b08a8479"
+    url "https://github.com/rrajesh1979/ref-java-cli/releases/download/v0.0.9/mychecksum-0.0.9-linux-x86_64.zip"
+    sha256 "2a9eae69ff3a090ce82c74b79271f5c128e2001188c0956e2ef8deac91d42916"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/rrajesh1979/ref-java-cli/releases/download/v0.0.7/mychecksum-0.0.7-osx-x86_64.zip"
-    sha256 "49d00faf2f1bbfd1452d6d2eff384ea3950d051d10c9a00aa5ddc5fcd34f553b"
+    url "https://github.com/rrajesh1979/ref-java-cli/releases/download/v0.0.9/mychecksum-0.0.9-osx-x86_64.zip"
+    sha256 "642460fcc942c6047e30fecd8ec890b4722da9d0292222393588e0cd151de0c0"
   end
 
 
@@ -22,6 +22,6 @@ class Mychecksum < Formula
 
   test do
     output = shell_output("#{bin}/mychecksum --version")
-    assert_match "0.0.7", output
+    assert_match "0.0.9", output
   end
 end
