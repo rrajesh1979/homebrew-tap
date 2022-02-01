12e356a4f0f7489e25dd8efd947895e2ed147dd0
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2022-01-31 19:19:30 -0500

    Create rmdbl.rb

---
+++ Formula/rmdbl.rb
@@ -0,0 +1,22 @@
+# Generated manually as a test
+class Rmdbload < Formula
+  desc "MongoDB Load gen"
+  homepage "https://github.com/rrajesh1979/rust_mdb_load"
+  version "0.0.13"
+  license "Apache-2.0"
+
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/rrajesh1979/rust_mdb_load/releases/download/v0.0.13/rust_mdb_load-0.0.13-x86_64-apple-darwin.zip"
+    sha256 "85435645c52651fa129f5db605b1dcbb86342f359d44daf59a720eb6e0c37712"
+  end
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink "#{libexec}/bin/rmdbload"
+  end
+
+  test do
+    output = shell_output("#{bin}/rmdbload --version")
+    assert_match "0.0.13", output
+  end
+end
