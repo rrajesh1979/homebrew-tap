d4653926d1b9fa9682aa1cf5b38fa6e8262e0fc5
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2022-02-01 14:34:42 -0500

    Update rust_mdb_load.rb

--- Formula/rust_mdb_load.rb
+++ Formula/rust_mdb_load.rb
@@ -2,12 +2,12 @@
 class RustMdbLoad < Formula
   desc "MongoDB Load gen 🦀"
   homepage "https://github.com/rrajesh1979/rust_mdb_load"
-  version "0.0.15"
+  version "0.0.16"
   license "Apache-2.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/rrajesh1979/rust_mdb_load/releases/download/v0.0.15/rust_mdb_load-0.0.15-x86_64-apple-darwin.zip"
-    sha256 "94a0e5e16afee75c4acfd405ec5e2d4f231c93fd581260bf4a6f21a4b47b7955"
+    url "https://github.com/rrajesh1979/rust_mdb_load/releases/download/v0.0.16/rust_mdb_load-0.0.16-x86_64-apple-darwin.zip"
+    sha256 "755e730d7a70d7c00097971cdc706dc17edb4ee28a02e8c3364ffc6bf31a08eb"
   end
 
   def install
@@ -17,6 +17,6 @@ class RustMdbLoad < Formula
 
   test do
     output = shell_output("#{bin}/rust_mdb_load --version")
-    assert_match "0.0.15", output
+    assert_match "0.0.16", output
   end
 end
